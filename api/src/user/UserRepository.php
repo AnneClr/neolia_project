62f57b74f4790d94146549825e9b772b710bb466
Yann <stoneroff@gmail.com>
2024-06-11 10:23:57 +0200

    rajout d'un script hash_password.php, dans userrepository, ajout de la méthode hash +comparaison du mdp hashé entre user et bdd

--- api/src/user/UserRepository.php
+++ api/src/user/UserRepository.php
@@ -19,13 +19,16 @@ class UserRepository {
         $this->dbInstance = DBAL::getConnection();
     }
 
+    public function hashPassword(string $password): string {
+        return password_hash($password, PASSWORD_BCRYPT);
+    }
+
     public function findByLoginAndPassword(string $username, string $password): ?UserEntity {
+        
         // Clean inputs
         $username = trim(htmlspecialchars($username, ENT_QUOTES, 'UTF-8'));
-        $password = trim(htmlspecialchars($password, ENT_QUOTES, 'UTF-8'));
 
-        // clean multiple spaces
-
+        // Clean multiple spaces
         $username = preg_replace('/\s+/', ' ', $username);
         $username = str_replace(' ', '', $username);
 
@@ -37,42 +40,45 @@ class UserRepository {
             JOIN user_has_role uhr ON u.id = uhr.user_id 
             JOIN role r ON uhr.role_id = r.id
             JOIN account a ON u.id = a.user_id 
-            WHERE login = :username AND password = :password;";
+            WHERE login = :username;";
         
         $pdoStatement = $this->dbInstance->prepare($sqlQuery);
         $pdoStatement->bindParam(':username', $username, \PDO::PARAM_STR);
-        $pdoStatement->bindParam(':password', $password, \PDO::PARAM_STR);
         $pdoStatement->execute();
 
         if ($pdoStatement) {
             $result = $pdoStatement->fetch(\PDO::FETCH_OBJ);
             if ($result) {
-                $user = new UserEntity();
-                $user->setId($result->userid);
-                $user->setLogin($result->login);
-                $user->setPassword($result->password);
+                if (password_verify($password, $result->password)) {
+                    $user = new UserEntity();
+                    $user->setId($result->userid);
+                    $user->setLogin($result->login);
+                    $user->setPassword($result->password);
 
-                $role = new RoleEntity();
-                $role->setId($result->roleid);
-                $role->setRole($result->role);
-                $user->addRole($role);
-
-                $account = new AccountEntity();
-                $account->setId($result->accountid);
-                $account->setLastname($result->lastname);
-                $account->setFirstname($result->firstname);
-                $account->setGender($result->gender);
-                $user->setAccount($account);
-                
-                while ($result = $pdoStatement->fetch(\PDO::FETCH_OBJ)) {
                     $role = new RoleEntity();
                     $role->setId($result->roleid);
                     $role->setRole($result->role);
-                    $user->addRole($role);                    
+                    $user->addRole($role);
+
+                    $account = new AccountEntity();
+                    $account->setId($result->accountid);
+                    $account->setLastname($result->lastname);
+                    $account->setFirstname($result->firstname);
+                    $account->setGender($result->gender);
+                    $user->setAccount($account);
+
+                    while ($result = $pdoStatement->fetch(\PDO::FETCH_OBJ)) {
+                        $role = new RoleEntity();
+                        $role->setId($result->roleid);
+                        $role->setRole($result->role);
+                        $user->addRole($role);                    
+                    }
+                    return $user;
+                } else {
+                    throw new NotFoundException('Incorrect password.');
                 }
-                return $user;
             } else {
-                throw new NotFoundException('No user were found with these credentials.');
+                throw new NotFoundException('No user found with this login.');
             }
         } else {
             throw new IncorrectSqlExpressionException('Something went wrong while processing query: ' . $sqlQuery);
